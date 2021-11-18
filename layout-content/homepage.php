d56a195dfea693ef4805545ce2970ebbfcc75217
MathElbo <mathijs.mg@hotmail.com>
2021-11-18 01:47:08 +0100

    Wat css en een nieuw logo

--- layout-content/homepage.php
+++ layout-content/homepage.php
@@ -1,8 +1,25 @@
 <div class="jumbotron jumbotron-fluid homeJumbo">
     <div class="container">
         <h1 class="display-4">woodklep.org</h1>
-        <p class="lead">woodklep studie tools</p>
-        <br>
-        <p>Allemaal coole toekomstige shit :)</p>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-6">
+                    <p class="lead">woodklep studie tools</p>
+                </div>
+
+                <div class="col-6">
+                    <div class="card" style="width: fit-content;">
+                        <img src="./assets/img/nlogo.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <p class="card-text">
+                                De woodklep heeft mystische schatten die jij kunt ontgrendellen!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     </div>
 </div>
