30bef4973b6c0250c6a93a69edceea137dbab212
qscholten <quig.scholten@outlook.com>
2021-11-25 14:15:42 +0100

    git

---
+++ layout-content/pricing.php
@@ -0,0 +1,14 @@
+<div class="jumbotron jumbotron-fluid homeJumbo">
+    <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                <h1 class="display-4">Pricing</h1>
+                    <p class="lead">De Woodklep kost geld.<br>
+                        Lorem ipsum dolor enzo.
+                    </p>
+                </div>                
+            </div>
+        </div>
+    </div>
+</div>
