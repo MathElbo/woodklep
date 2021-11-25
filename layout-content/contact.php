5ff128809763440efa8bad30e2754b3844f17b28
qscholten <quig.scholten@outlook.com>
2021-11-25 20:12:26 +0100

    Resize van invoerformulier

--- layout-content/contact.php
+++ layout-content/contact.php
@@ -6,9 +6,9 @@
         <p>Onze denkbeeldige contact gegevens:</p>
         <form action="index.php?content=script-contact" method="post">
           <div class="form-group">
-            <input class="form-control" type="name" name="name" id="name" placeholder="Name" required>
-            <input class="form-control" type="email" name="email" id="email" placeholder="E-mail" required>
-            <textarea class="form-control" type="bericht" name="bericht" id="bericht" placeholder="Bericht" rows="5" cols="40" required></textarea>
+            <input class="form-control" style="width:570px" type="name" name="name" id="name" placeholder="Name" required>
+            <input class="form-control" style="width:570px" type="email" name="email" id="email" placeholder="E-mail" required>
+            <textarea class="form-control" style="width:570px" type="bericht" name="bericht" id="bericht" placeholder="Bericht" rows="5" cols="40" required></textarea>
             <input class="btn btn-dark" type="submit" value="Verstuur">
           </div>
         </form>
