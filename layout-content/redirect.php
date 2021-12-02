d979ad05e1f53624167d6eb6f9f11fbb0084f265
MathElbo <mathijs.mg@hotmail.com>
2021-12-02 10:31:32 +0100

    pre cancer merge

--- layout-content/redirect.php
+++ layout-content/redirect.php
@@ -3,13 +3,13 @@
 if (isset($_SESSION["userrole"]))
   switch ($_SESSION["userrole"]) {
     case "1":
-      header("Location: index.php?content=myaccount");
+      header("Location: index.php?content=studenthome");
       break;
     case "2":
-      header("Location: index.php?content=myaccount");
+      header("Location: index.php?content=parenthome");
       break;
     case "3":
-      header("Location: index.php?content=myaccount");
+      header("Location: index.php?content=parenthome");
       break;
     case "4":
       header("Location: index.php?content=myaccount");
---
+++ layout-content/studenthome.php
@@ -0,0 +1,46 @@
+<div class="jumbotron jumbotron-fluid homeJumbo">
+    <div class="container">
+        <h1 class="display-4">student</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="row">
+
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Mijn huiswerk</h5>
+                                    <p class="card-text">Zie hier Huiswerk overzicht.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                            <br>
+                        </div>
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Volgende opdracht</h5>
+                                    <p class="card-text">.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card" style="width: fit-content;">
+                        <img src="./assets/img/nlogo.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <p class="card-text">
+                                De woodklep heeft mystische schatten die jij kunt ontgrendellen!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
