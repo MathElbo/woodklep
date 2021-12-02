886ac77d92651c6f0916876862b4faa497b016e0
MathElbo <mathijs.mg@hotmail.com>
2021-12-02 10:33:35 +0100

    homepage gerevert redirects klaargezet

--- layout-content/homepage.php
+++ layout-content/homepage.php
@@ -1,48 +1,23 @@
-    <div class="jumbotron jumbotron-fluid homepagecolor">
-        <div class="row">
-
-            <div class="rechts col-lg-6 container">
-                <div class="image links">
-                    <div class="items ">
-
-                        <img class="picture" src="https://cdn.vox-cdn.com/thumbor/6WqH2YyoREHRx26bCCk1Yo04L9w=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/11701871/ive.png" alt="niks">
-                        <div class="container text2">sdadsadsdsadsaoidjasoidsdsadsalidjsaiodjasiodsaiojdioasjdiosajdiosajdoisajiodsajiodasijoajoidijoasdiosajdiosajiojsaadsa
-
-                        </div>
-
-
-                    </div>
-
-                    <div class="items ">
-
-                        <img class="picture" src="https://cdn.vox-cdn.com/thumbor/6WqH2YyoREHRx26bCCk1Yo04L9w=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/11701871/ive.png" alt="niks">
-                        <div class="container text2">sdadsadsdsadsaoidjasoidsdsadsalidjsaiodjasiodsaiojdioasjdiosajdiosajdoisajiodsajiodasijoajoidijoasdiosajdiosajiojsaadsa
-
-                        </div>
-
-
-                    </div>
-
-
-
-                    <div class="items ">
-
-                        <img class="picture" src="https://cdn.vox-cdn.com/thumbor/6WqH2YyoREHRx26bCCk1Yo04L9w=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/11701871/ive.png" alt="niks">
-                        <div class="container text2">sdadsadsdsadsaoidjasoidsdsadsalidjsaiodjasiodsaiojdioasjdiosajdiosajdoisajiodsajiodasijoajoidijoasdiosajdiosajiojsaadsa
-
-                        </div>
-
-
-                    </div>
-
-
-
+<div class="jumbotron jumbotron-fluid homeJumbo">
+    <div class="container">
+        <h1 class="display-4">woodklep.org</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <p class="lead">woodklep studie tools</p>
                 </div>
-
+                <div class="col-6">
+                    <div class="card" style="width: fit-content;">
+                        <img src="./assets/img/nlogo.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <p class="card-text">
+                                De woodklep heeft mystische schatten die jij kunt ontgrendellen!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
-            <div class=" rechts col-lg-6">
-                <img src="./assets/img/nlogo.jpg" class="image">
-            </div>
-
         </div>
     </div>
+</div>
