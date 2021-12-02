1adc2537dc3282f76007392f259ceebea64b3ed7
qscholten <quig.scholten@outlook.com>
2021-12-02 11:02:26 +0100

    Change in navbar (logo + home to student, teacher and parant home)

--- layout-basic/navbar.php
+++ layout-basic/navbar.php
@@ -4,13 +4,43 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-  <a class="navbar-brand" href="./index.php?content=homepage">
-    <div class="navLogoText">
+  <?php
+  if (isset($_SESSION["userrole"])){
+    switch($_SESSION["userrole"]) {
+      case "1": echo '<a class="navbar-brand" href="./index.php?content=studenthome"><div class="navLogoText">
+        Woodklep.org
+        <img src="./assets/img/nlogo.jpg" class="d-inline-block align-top navLogo">
+        </div>
+        </a>';
+      break;
+      case "2": echo '<a class="navbar-brand" href="./index.php?content=parenthome"><div class="navLogoText">
+        Woodklep.org
+        <img src="./assets/img/nlogo.jpg" class="d-inline-block align-top navLogo">
+        </div>
+        </a>';
+      break;
+      case "3": echo '<a class="navbar-brand" href="./index.php?content=teacherhome"><div class="navLogoText">
+        Woodklep.org
+        <img src="./assets/img/nlogo.jpg" class="d-inline-block align-top navLogo">
+        </div>
+        </a>';
+      break;
+      default: echo '<a class="navbar-brand" href="./index.php?content=homepage"><div class="navLogoText">
+        Woodklep.org
+        <img src="./assets/img/nlogo.jpg" class="d-inline-block align-top navLogo">
+        </div>
+        </a>';
+      break;
+    }
+  }
+  else {
+    echo '<a class="navbar-brand" href="./index.php?content=homepage"><div class="navLogoText">
       Woodklep.org
       <img src="./assets/img/nlogo.jpg" class="d-inline-block align-top navLogo">
-
-    </div>
-  </a>
+      </div>
+      </a>';
+  }
+  ?>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -18,8 +48,27 @@
 
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="<?php if ($content == 'homepage') echo 'active' ?> nav-item">
-        <a class="nav-link" href="./index.php?content=homepage">Home</a></li>
+      <?php
+      if (isset($_SESSION["userrole"])) {
+        switch ($_SESSION["userrole"]) {
+          case "1":
+            echo "<li><a class='nav-link' href='index.php?content=studenthome'>Home</a></li>";
+            break;
+          case "2":
+            echo "<li><a class='nav-link' href='index.php?content=parenthome'>Home</a></li>";
+            break;
+          case "3":
+            echo "<li><a class='nav-link' href='index.php?content=teacherhome'>Home</a></li>";
+            break;
+          default: 
+            echo "<li><a class='nav-link' href='index.php?content=homepage'>Home</a></li>";
+            break;
+        }
+      }
+      else {
+        echo "<li><a class='nav-link' href='index.php?content=homepage'>Home</a></li>";
+      }
+      ?>
       <li class="<?php if ($content == 'contact') echo 'active' ?> nav-item">
         <a class="nav-link" href="index.php?content=contact">Contact</a></li>
         <li class="<?php if ($content == 'overons') echo 'active' ?> nav-item">
@@ -37,22 +86,6 @@
       <li><a class='nav-link' href='index.php?content=myaccount'>Mijn account</a></li>
       <li><a class='nav-link' href='index.php?content=uitloggen'>Uitloggen</a></li>
       ";
-
-      switch ($_SESSION["userrole"]) {
-        case "1":
-          echo "<li><a class='nav-link' href='index.php?content=studenthome'>Studenten Home</a></li>";
-          break;
-        case "2":
-          echo "<li><a class='nav-link' href='index.php?content=parenthome'>Ouders Home</a></li>";
-          break;
-        case "3":
-          echo "<li><a class='nav-link' href='index.php?content=teacherhome'>Docenten Home</a></li>";
-          break;
-        case "4":
-          header("Location: index.php?content=myaccount");
-          break;
-      }
-
       }
       ?> 
     </ul>
