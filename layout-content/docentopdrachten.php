daf282f61178c4fd5f4d4587fcf6b291a1c55387
qscholten <quig.scholten@outlook.com>
2021-12-08 16:46:17 +0100

    Kleine aanpassingen in oa comments

--- layout-content/docentopdrachten.php
+++ layout-content/docentopdrachten.php
@@ -1,6 +1,8 @@
 <?php
 $userrole = [3,4];
 include("./php-scripts/security.php");
+include("./php-scripts/connectDB.php");
+$id = $_SESSION['id'];
 ?>
 
 <div class="jumbotron jumbotron-fluid homeJumbo">
@@ -9,6 +11,8 @@ include("./php-scripts/security.php");
             <div class="col-6">
                 <h1 class="display-4">Mijn Opdrachten</h1>
                     <p class="lead">Mijn Opdrachten</p>
+                    <?php
+                    ?>
             </div>                
         </div>
     </div>
